72f6593c2c7ea949b0d7ba5808d3ae17b6fe9c2d
Shen-An <2021902023@chd.edu.cn>
2025-01-01 12:46:11 +0800

    first commit

---
+++ easychat-java/src/main/java/com/easychat/EasyChatApplication.java
@@ -0,0 +1,7 @@
+package com.easychat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.easychat")
+public class EasyChatApplication {
+}
