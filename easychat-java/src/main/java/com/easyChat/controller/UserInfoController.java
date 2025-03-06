c1b93925f24cd362627ced05a7409c011729900b
Shen-An <2021902023@chd.edu.cn>
2025-03-06 13:16:09 +0800

    完成了getUserInfo接口

--- easychat-java/src/main/java/com/easyChat/controller/UserInfoController.java
+++ easychat-java/src/main/java/com/easyChat/controller/UserInfoController.java
@@ -1,15 +1,20 @@
 package com.easyChat.controller;
 
+import com.easyChat.anotation.GlobalInterceptor;
+import com.easyChat.entity.dto.TokenUserInfoDto;
 import com.easyChat.entity.vo.ResponseVo;
 import com.easyChat.entity.po.UserInfo;
 import com.easyChat.entity.query.UserInfoQuery;
+import com.easyChat.entity.vo.UserInfoVo;
 import com.easyChat.service.UserInfoService;
 
+import com.easyChat.utils.CopyUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -116,5 +121,13 @@ public class UserInfoController extends ABaseController {
 		return getSuccessResponseVo(null);
 	}
 
-
+	@GlobalInterceptor
+	@RequestMapping("/getUserInfo")
+	public ResponseVo getUserInfo(HttpServletRequest request) {
+		TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+		UserInfo userInfo = userInfoService.getUserInfoByUserId(tokenUserInfoDto.getUserId());
+		UserInfoVo userInfoVo = CopyUtils.copy(userInfo,UserInfoVo.class);
+		userInfoVo.setAdmin(tokenUserInfoDto.getAdmin());
+		return getSuccessResponseVo(userInfoVo);
+	}
 }
