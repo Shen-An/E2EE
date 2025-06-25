6bd202c52313fa39dad65f5252b68b1813a7d6cb
Shen-An <2021902023@chd.edu.cn>
2025-06-25 21:59:54 +0800

    新增系统提示消息

---
+++ easychat-ui/easychat-front/src/renderer/src/views/chat/ChatMessageSys.vue
@@ -0,0 +1,38 @@
+<template>
+  <ChatMessageTime :data="data"></ChatMessageTime>
+  <div class="sys-message">
+    {{ dataMessage.messageContent }}
+  </div>
+</template>
+
+<script setup>
+import ChatMessageTime from './ChatMessageTime.vue'
+import { ref, reactive, getCurrentInstance, nextTick, computed } from 'vue'
+const { proxy } = getCurrentInstance()
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: {}
+  }
+})
+
+const dataMessage = computed(()=>{
+    const data = {...props.data}
+    if(data.messageType == 1){
+        data.messageContent = '我们已经成功添加为好友，可以开始聊天啦~'
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.sys-message {
+  text-align: center;
+  color: #999999;
+  font-size: 12px;
+  margin-bottom: 20px;
+}
+</style>
