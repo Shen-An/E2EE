4d3fb0c7dd7233b36d449de4276c4e15f26d5a6c
Shen-An <2021902023@chd.edu.cn>
2025-06-29 14:27:16 +0800

    群详情

---
+++ easychat-ui/easychat-front/src/renderer/src/views/chat/ChatGroupDetail.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="group-panel">
+    <el-drawer v-model="showDrawer" model-class="mask-style" :size="300" ref="drawerRef">
+      <div class="group-panel-body">
+        <div class="member-list">
+          <div class="member-item" v-for="item in memberList" :key="item.userId">
+            <Avatar :userId="item.userId" :width="30" @closeDrawer="closeDrawerHandler"></Avatar>
+            <div class="nick-name" :title="item.contactName">{{ item.contactName }}</div>
+            <div class="owner-tag" v-if="item.userId == groupInfo.groupOwnerId">群主</div>
+          </div>
+          <template v-if="userInfoStore.getInfo().userId == groupInfo.groupOwnerId">
+            <div class="member-item" @click="addUser">
+              <div class="iconfont icon-add icon-op"></div>
+              <div class="nick-name">添加</div>
+            </div>
+            <div class="member-item" @click="removeUser">
+              <div class="iconfont icon-min icon-op"></div>
+              <div class="nick-name">移除</div>
+            </div>
+          </template>
+        </div>
+        <div class="line"></div>
+        <div class="part-content">
+          <AvatarBase :userId="groupInfo.groupId" :width="60" :borderRadius="5" :showDetail="true">
+          </AvatarBase>
+        </div>
+        <div class="part-title">群号</div>
+        <div class="part-content">{{ groupInfo.groupId }}</div>
+        <div class="part-title">群名称</div>
+        <div class="part-content">{{ groupInfo.groupName }}</div>
+        <div class="part-title">群公告</div>
+        <div class="part-content">{{ groupInfo.groupNotice || '-' }}</div>
+        <div class="line"></div>
+        <a
+          href="javascript:void(0);"
+          class="leave-btn"
+          @click="dissolutionGroup"
+          v-if="userInfoStore.getInfo().userId == groupInfo.groupOwnerId"
+          >解散群聊</a
+        >
+        <a href="javascript:void(0);" class="leave-btn" @click="leaveGroup" v-else>退出群聊</a>
+      </div>
+    </el-drawer>
+    <UserSelect ref="userSelectRef" @callback="addOrRemoveUserCallback"></UserSelect>
+  </div>
+</template>
+<script setup>
+import UserSelect from './UserSelect.vue'
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance()
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+import { useUserInfoStore } from '@/stores/UserInfoStore'
+const userInfoStore = useUserInfoStore()
+
+import { useContactStateStore } from '@/stores/ContactStateStore'
+const contactStateStore = useContactStateStore()
+
+const showDrawer = ref(false)
+const memberList = ref([])
+const groupInfo = ref({})
+
+const show = async (groupId) => {
+  
+  let resp = await proxy.Request({
+    url: proxy.Api.getGroupInfo4Chat,
+    params: {
+      groupId: groupId
+    },
+    showError: false,
+    errorCallback: (response) => {
+      proxy.Confirm({ message: response.info, showCancelBtn: false })
+    }
+  })
+  if (!resp) {
+    return
+  }
+  showDrawer.value = true
+  memberList.value = resp.data.userContactList
+  groupInfo.value = resp.data.groupInfo
+}
+
+defineExpose({
+  show
+})
+
+const drawerRef = ref(null)
+const closeDrawerHandler = () => {
+  drawerRef.value.close()
+}
+
+// 添加群成员
+const userSelectRef = ref()
+const addUser = async () => {
+  let resp = await proxy.Request({
+    url: proxy.Api.loadContact,
+    params: {
+      contactType: 'USER'
+    }
+  })
+  if (!resp) {
+    return
+  }
+  const contactIds = memberList.value.map((item) => item['userId'])
+
+  let contactList = resp.data
+  console.log(contactList)
+  contactList.forEach((element) => {
+    if (contactIds.includes(element.contactId)) {
+      element.disabled = true
+    }
+  })
+  userSelectRef.value.show({
+    contactList,
+    groupId: groupInfo.value.groupId,
+    opType: 1
+  })
+
+}
+
+// 添加或移除群成员回调
+const removeUser =  () => {
+  let contactList = memberList.value.map((item) => item)
+  contactList.forEach((item) => {
+    item.contactId == item.userId
+  })
+  contactList.splice(0, 1)
+  userSelectRef.value.show({
+    contactList,
+    groupId: groupInfo.value.groupId,
+    opType: 0
+  })
+}
+
+const addOrRemoveUserCallback = () => {
+  showDrawer.value = false 
+}
+</script>
+
+<style lang="scss" scoped>
+.group-panel {
+  color: #000000;
+  :deep(.mask-style) {
+    top: 1px;
+    right: 1px;
+    height: calc(100vh - 2px);
+  }
+  :deep(.el-drawer) {
+    -webkit-app-region: no-drag;
+  }
+  :deep(.el-drawer__header) {
+    margin-bottom: 10px;
+  }
+  :deep(.el-drawer__body) {
+    padding: 10px;
+  }
+  .group-panel-body {
+    .member-list {
+      display: flex;
+      flex-wrap: wrap;
+      .member-item {
+        width: 20%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 5px;
+        padding: 5px;
+        position: relative;
+        .owner-tag {
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          font-size: 12px;
+          background: #07c160;
+          color: #fff;
+          border-radius: 3px;
+        }
+        .nick-name {
+          margin-top: 3px;
+          width: 100%;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          font-size: 13px;
+          text-align: center;
+        }
+        .icon-op {
+          cursor: pointer;
+          width: 30px;
+          height: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 1px solid #dbdbdb;
+          color: #6e6e6e;
+        }
+      }
+    }
+    .line {
+      margin-bottom: 10px;
+      margin-top: 1px solid #ddd;
+      height: 1px;
+    }
+    .part-title {
+      margin-top: 10px;
+    }
+    .part-content {
+      color: #757575;
+      margin-bottom: 10px;
+    }
+    .leave-btn {
+      color: #f45454;
+      text-decoration: none;
+      text-align: center;
+      display: block;
+      margin-top: 10px;
+    }
+  }
+}
+</style>
