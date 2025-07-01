9ba1aae9ed698a01102f66ff903a64df5a36731e
Shen-An <2021902023@chd.edu.cn>
2025-07-01 16:20:54 +0800

    添加了dissolutionGroup，leaveGroup

--- easychat-java/src/main/java/com/easyChat/controller/GroupInfoController.java
+++ easychat-java/src/main/java/com/easyChat/controller/GroupInfoController.java
@@ -9,6 +9,7 @@ import com.easyChat.entity.vo.ResponseVo;
 import com.easyChat.entity.po.GroupInfo;
 import com.easyChat.entity.query.GroupInfoQuery;
 import com.easyChat.enums.GroupStatusEnum;
+import com.easyChat.enums.MessageTypeEnum;
 import com.easyChat.enums.UserContactStatusEnum;
 import com.easyChat.exception.BusinessException;
 import com.easyChat.service.GroupInfoService;
@@ -35,190 +36,215 @@ import java.util.List;
 @RequestMapping("/groupInfo")
 public class GroupInfoController extends ABaseController {
 
-	@Resource
-	private GroupInfoService groupInfoService;
-	@Resource
-	private UserContactService userContactService;
+    @Resource
+    private GroupInfoService groupInfoService;
+    @Resource
+    private UserContactService userContactService;
 
-	@RequestMapping("loadDataList")
-	public ResponseVo loadDataList(GroupInfoQuery query) {
-		return getSuccessResponseVo(groupInfoService.findListByPage(query));
-	}
-	/**
-	 * 新增
-	 */
+    @RequestMapping("loadDataList")
+    public ResponseVo loadDataList(GroupInfoQuery query) {
+        return getSuccessResponseVo(groupInfoService.findListByPage(query));
+    }
 
-	@RequestMapping("add")
-	public ResponseVo add(GroupInfo bean) {
-		this.groupInfoService.add(bean);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 新增
+     */
 
-	/**
-	 * 批量新增
-	 */
+    @RequestMapping("add")
+    public ResponseVo add(GroupInfo bean) {
+        this.groupInfoService.add(bean);
+        return getSuccessResponseVo(null);
+    }
 
-	@RequestMapping("addBatch")
-	public ResponseVo addBatch(@RequestBody List<GroupInfo> listBean) {
-		this.groupInfoService.addBatch(listBean);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 批量新增
+     */
 
-	/**
-	 * 批量新增或修改
-	 */
+    @RequestMapping("addBatch")
+    public ResponseVo addBatch(@RequestBody List<GroupInfo> listBean) {
+        this.groupInfoService.addBatch(listBean);
+        return getSuccessResponseVo(null);
+    }
 
-	@RequestMapping("addOrUpdateBatch")
-	public ResponseVo addOrUpdateBatch(@RequestBody List<GroupInfo> listBean) {
-		this.groupInfoService.addOrUpdateBatch(listBean);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 批量新增或修改
+     */
 
-	/**
-	 * 根据GroupId查询
-	 */
+    @RequestMapping("addOrUpdateBatch")
+    public ResponseVo addOrUpdateBatch(@RequestBody List<GroupInfo> listBean) {
+        this.groupInfoService.addOrUpdateBatch(listBean);
+        return getSuccessResponseVo(null);
+    }
 
-	@RequestMapping("getGroupInfoByGroupId")
-	public ResponseVo getGroupInfoByGroupId(String groupId) {
-		return getSuccessResponseVo(this.groupInfoService.getGroupInfoByGroupId(groupId));
-	}
+    /**
+     * 根据GroupId查询
+     */
 
-	/**
-	 * 根据GroupId更新
-	 */
+    @RequestMapping("getGroupInfoByGroupId")
+    public ResponseVo getGroupInfoByGroupId(String groupId) {
+        return getSuccessResponseVo(this.groupInfoService.getGroupInfoByGroupId(groupId));
+    }
 
-	@RequestMapping("updateGroupInfoByGroupId")
-	public ResponseVo updateGroupInfoByGroupId(GroupInfo bean, String groupId) {
-		this.groupInfoService.updateGroupInfoByGroupId(bean,groupId);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 根据GroupId更新
+     */
 
-	/**
-	 * 根据GroupId删除
-	 */
+    @RequestMapping("updateGroupInfoByGroupId")
+    public ResponseVo updateGroupInfoByGroupId(GroupInfo bean, String groupId) {
+        this.groupInfoService.updateGroupInfoByGroupId(bean, groupId);
+        return getSuccessResponseVo(null);
+    }
 
-	@RequestMapping("deleteGroupInfoByGroupId")
-	public ResponseVo deleteGroupInfoByGroupId(String groupId) {
-		this.groupInfoService.deleteGroupInfoByGroupId(groupId);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 根据GroupId删除
+     */
 
-	/**
-	 *保存/更新群聊
-	 * @param request
-	 * @param groupId
-	 * @param groupName
-	 * @param groupNotice
-	 * @param joinType
-	 * @param avatarFile
-	 * @param avatarCover
-	 * @return
-	 * @throws IOException
-	 */
-	@GlobalInterceptor
-	@RequestMapping("/saveGroup")
-	public ResponseVo saveGroup(HttpServletRequest request,
-								String groupId,
-								@NotEmpty String groupName,
-								String groupNotice,
-								@NotNull Integer joinType,
-								MultipartFile avatarFile,
-								MultipartFile avatarCover
-								) throws IOException {
-		TokenUserInfoDto TokenUserInfoDto = getTokenUserInfo(request);
-		GroupInfo groupInfo = new GroupInfo();
-		groupInfo.setGroupId(groupId);
-		groupInfo.setGroupName(groupName);
-		groupInfo.setGroupNotice(groupNotice);
-		groupInfo.setJoinType(joinType);
-		groupInfo.setGroupOwnerId(TokenUserInfoDto.getUserId());
+    @RequestMapping("deleteGroupInfoByGroupId")
+    public ResponseVo deleteGroupInfoByGroupId(String groupId) {
+        this.groupInfoService.deleteGroupInfoByGroupId(groupId);
+        return getSuccessResponseVo(null);
+    }
 
-		groupInfoService.saveGroup(groupInfo,avatarFile,avatarCover);
-		return getSuccessResponseVo(null);
-	}
+    /**
+     * 保存/更新群聊
+     *
+     * @param request
+     * @param groupId
+     * @param groupName
+     * @param groupNotice
+     * @param joinType
+     * @param avatarFile
+     * @param avatarCover
+     * @return
+     * @throws IOException
+     */
+    @GlobalInterceptor
+    @RequestMapping("/saveGroup")
+    public ResponseVo saveGroup(HttpServletRequest request,
+                                String groupId,
+                                @NotEmpty String groupName,
+                                String groupNotice,
+                                @NotNull Integer joinType,
+                                MultipartFile avatarFile,
+                                MultipartFile avatarCover
+    ) throws IOException {
+        TokenUserInfoDto TokenUserInfoDto = getTokenUserInfo(request);
+        GroupInfo groupInfo = new GroupInfo();
+        groupInfo.setGroupId(groupId);
+        groupInfo.setGroupName(groupName);
+        groupInfo.setGroupNotice(groupNotice);
+        groupInfo.setJoinType(joinType);
+        groupInfo.setGroupOwnerId(TokenUserInfoDto.getUserId());
 
-	/**
-	 * 加载我的群聊
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping("/loadMyGroup")
-	@GlobalInterceptor
-	public ResponseVo loadMyGroup(HttpServletRequest request) {
-		TokenUserInfoDto TokenUserInfoDto = getTokenUserInfo(request);
-		GroupInfoQuery groupInfoQuery = new GroupInfoQuery();
-		groupInfoQuery.setGroupOwnerId(TokenUserInfoDto.getUserId());
-		groupInfoQuery.setOrderBy("create_time asc");
-		List<GroupInfo>list = this.groupInfoService.findListByParam(groupInfoQuery);
-		return getSuccessResponseVo(list);
-	}
+        groupInfoService.saveGroup(groupInfo, avatarFile, avatarCover);
+        return getSuccessResponseVo(null);
+    }
 
-	/**
-	 * 获取群聊详细信息
-	 * @param request
-	 * @param groupId
-	 * @return
-	 */
-	@RequestMapping("/getGroupInfo")
-	@GlobalInterceptor
-	public ResponseVo getGroupInfo(HttpServletRequest request,@NotEmpty String groupId) {
-		GroupInfo groupInfo = getGroupDetailCommon(request, groupId);
-		UserContactQuery userContactQuery = new UserContactQuery();
-		userContactQuery.setContactId(groupId);
-		userContactQuery.setOrderBy("create_time asc");
-		Integer memberCount = userContactService.findCountByParam(userContactQuery);
-		groupInfo.setMemberCount(memberCount);
-		return getSuccessResponseVo(groupInfo);
-	}
+    /**
+     * 加载我的群聊
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("/loadMyGroup")
+    @GlobalInterceptor
+    public ResponseVo loadMyGroup(HttpServletRequest request) {
+        TokenUserInfoDto TokenUserInfoDto = getTokenUserInfo(request);
+        GroupInfoQuery groupInfoQuery = new GroupInfoQuery();
+        groupInfoQuery.setGroupOwnerId(TokenUserInfoDto.getUserId());
+        groupInfoQuery.setOrderBy("create_time asc");
+        List<GroupInfo> list = this.groupInfoService.findListByParam(groupInfoQuery);
+        return getSuccessResponseVo(list);
+    }
 
-	/**
-	 * 查群组和联系人的信息
-	 * @param request
-	 * @param groupId
-	 * @return
-	 */
-	@RequestMapping("/getGroupInfo4Chat")
-	@GlobalInterceptor
-	public ResponseVo getGroupInfo4Chat(HttpServletRequest request,@NotEmpty String groupId) {
-		GroupInfo groupInfo = getGroupDetailCommon(request, groupId);
-		UserContactQuery userContactQuery = new UserContactQuery();
-		userContactQuery.setContactId(groupId);
-		userContactQuery.setQueryUserInfo(true);
-		userContactQuery.setOrderBy("create_time asc");
-		userContactQuery.setStatus(UserContactStatusEnum.FRIEND.getStatus());
-		List<UserContact> userContactList = this.userContactService.findListByParam(userContactQuery);
-		GroupInfoVo groupInfoVo = new GroupInfoVo();
-		groupInfoVo.setGroupInfo(groupInfo);
-		groupInfoVo.setUserContactList(userContactList);
-		return getSuccessResponseVo(groupInfoVo);
-	}
+    /**
+     * 获取群聊详细信息
+     *
+     * @param request
+     * @param groupId
+     * @return
+     */
+    @RequestMapping("/getGroupInfo")
+    @GlobalInterceptor
+    public ResponseVo getGroupInfo(HttpServletRequest request, @NotEmpty String groupId) {
+        GroupInfo groupInfo = getGroupDetailCommon(request, groupId);
+        UserContactQuery userContactQuery = new UserContactQuery();
+        userContactQuery.setContactId(groupId);
+        userContactQuery.setOrderBy("create_time asc");
+        Integer memberCount = userContactService.findCountByParam(userContactQuery);
+        groupInfo.setMemberCount(memberCount);
+        return getSuccessResponseVo(groupInfo);
+    }
+
+    /**
+     * 查群组和联系人的信息
+     *
+     * @param request
+     * @param groupId
+     * @return
+     */
+    @RequestMapping("/getGroupInfo4Chat")
+    @GlobalInterceptor
+    public ResponseVo getGroupInfo4Chat(HttpServletRequest request, @NotEmpty String groupId) {
+        GroupInfo groupInfo = getGroupDetailCommon(request, groupId);
+        UserContactQuery userContactQuery = new UserContactQuery();
+        userContactQuery.setContactId(groupId);
+        userContactQuery.setQueryUserInfo(true);
+        userContactQuery.setOrderBy("create_time asc");
+        userContactQuery.setStatus(UserContactStatusEnum.FRIEND.getStatus());
+        List<UserContact> userContactList = this.userContactService.findListByParam(userContactQuery);
+        GroupInfoVo groupInfoVo = new GroupInfoVo();
+        groupInfoVo.setGroupInfo(groupInfo);
+        groupInfoVo.setUserContactList(userContactList);
+        return getSuccessResponseVo(groupInfoVo);
+    }
 
 
-	private GroupInfo getGroupDetailCommon(HttpServletRequest request,String groupId) {
-		TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+    private GroupInfo getGroupDetailCommon(HttpServletRequest request, String groupId) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
 
-		//判断是否是本群组人员的请求
-		UserContact userContact = userContactService.getUserContactByUserIdAndContactId(tokenUserInfoDto.getUserId(),groupId);
-		if(userContact == null || UserContactStatusEnum.FRIEND.equals(userContact.getStatus())){
-			throw new BusinessException("你不在群聊中/群聊不存在/群聊已解散");
-		}
+        //判断是否是本群组人员的请求
+        UserContact userContact = userContactService.getUserContactByUserIdAndContactId(tokenUserInfoDto.getUserId(), groupId);
+        if (userContact == null || UserContactStatusEnum.FRIEND.equals(userContact.getStatus())) {
+            throw new BusinessException("你不在群聊中/群聊不存在/群聊已解散");
+        }
 
-		GroupInfo groupInfo = this.groupInfoService.getGroupInfoByGroupId(groupId);
-		//判断是否群聊正常
-		if(groupInfo == null || !GroupStatusEnum.NORMAL.getStatus().equals(groupInfo.getStatus())){
-			throw new BusinessException("群聊不存在/群聊已解散");
-		}
-		return groupInfo;
-	}
+        GroupInfo groupInfo = this.groupInfoService.getGroupInfoByGroupId(groupId);
+        //判断是否群聊正常
+        if (groupInfo == null || !GroupStatusEnum.NORMAL.getStatus().equals(groupInfo.getStatus())) {
+            throw new BusinessException("群聊不存在/群聊已解散");
+        }
+        return groupInfo;
+    }
 
-	@RequestMapping("/addOrRemoveGroupUser")
-	@GlobalInterceptor()
-	public ResponseVo addOrRemoveGroupUser(HttpServletRequest request,
-									 @NotEmpty String groupId,
-			  					     @NotEmpty String selectContacts,
-									 @NotNull Integer opType) {
-		TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
-		groupInfoService.addOrRemoveGroupUser(tokenUserInfoDto,groupId,selectContacts,opType);
-		return getSuccessResponseVo(null);
-	}
+    @RequestMapping("/addOrRemoveGroupUser")
+    @GlobalInterceptor()
+    public ResponseVo addOrRemoveGroupUser(HttpServletRequest request,
+                                           @NotEmpty String groupId,
+                                           @NotEmpty String selectContacts,
+                                           @NotNull Integer opType) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        groupInfoService.addOrRemoveGroupUser(tokenUserInfoDto, groupId, selectContacts, opType);
+        return getSuccessResponseVo(null);
+    }
+
+    @RequestMapping("/leaveGroup")
+    @GlobalInterceptor()
+    public ResponseVo leaveGroup(HttpServletRequest request,
+                                           @NotEmpty String groupId
+                                           ) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        groupInfoService.leaveGroup(tokenUserInfoDto.getUserId(),groupId, MessageTypeEnum.LEAVE_GROUP);
+        return getSuccessResponseVo(null);
+    }
+
+    @RequestMapping("/dissolutionGroup")
+    @GlobalInterceptor()
+    public ResponseVo dissolutionGroup(HttpServletRequest request,
+                                           @NotEmpty String groupId
+    ) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        groupInfoService.dissolutionGroup(tokenUserInfoDto.getUserId(),groupId);
+        return getSuccessResponseVo(null);
+    }
 }
